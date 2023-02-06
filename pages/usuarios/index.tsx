babe3e5ce0eafad785e7e3a623aa7a2f58cf2939
codethebasics <codethebasics@gmail.com>
2023-02-06 16:56:32 -0300

    status label

--- pages/usuarios/index.tsx
+++ pages/usuarios/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import styles from '@/styles/usuarios/UsersPage.module.scss'
 import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch'
 import Image from 'next/image'
+import AddIcon from '@mui/icons-material/Add'
 
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
@@ -13,19 +14,28 @@ type User = {
   active: string
 }
 
-const SearchInput = () => {
-  return <input type="text" placeholder="Pesquisar por nome ou e-mail" />
-}
-
-const UserStatusBadge = ({ status }: any) => {
-  return status.toLowerCase() === 'active' ? (
-    <p className={`${styles.badge} ${styles.active}`}>&#10004;</p>
-  ) : (
-    <p className={`${styles.badge} ${styles.inactive}`}>&#10006;</p>
+const SearchInput = ({ text, onChange }: any) => {
+  return (
+    <input
+      type="text"
+      placeholder="Pesquisar por nome ou e-mail"
+      value={text}
+      onChange={onChange}
+    />
   )
 }
 
-export const CardUser = ({ id, name, email, img }) => {
+const UserStatusBadge = ({ status }: any) => {
+  return (
+    <div className={`${styles.statusBadge}`}>
+      <div className={`${styles[status.toLowerCase()]}`}>
+        {status.toLowerCase() === 'active' ? 'ativo' : 'inativo'}
+      </div>
+    </div>
+  )
+}
+
+export const CardUser = ({ id, name, email, img, active }: any) => {
   return (
     <div className={styles.cardUser}>
       <div className={styles.avatar}>
@@ -35,6 +45,7 @@ export const CardUser = ({ id, name, email, img }) => {
           width={75}
           height={75}
         />
+        <UserStatusBadge status={active} />
       </div>
       <div className={styles.info}>
         <div>{name}</div>
@@ -50,28 +61,30 @@ export const CardUser = ({ id, name, email, img }) => {
   )
 }
 
-export const UserList = ({ usuarios }: any) => {
+export const UserList = ({ usuarios, filter }: any) => {
+  const _filter = filter.toLowerCase().trim()
+
   return (
     <div className={styles.userListContainer}>
-      {usuarios.map(usuario => (
-        // <Link
-        //   href={`${process.env.NEXT_PUBLIC_APP_URL}/usuarios/${usuario.id}`}
-        //   key={usuario.id}
-        // >
-        <CardUser
-          id={usuario.id}
-          name={usuario.name}
-          email={usuario.email}
-          img={Math.floor(Math.random() * 100)}
-        />
-        // </Link>
-      ))}
+      {usuarios
+        .filter((usuario: any) => usuario.name.toLowerCase().includes(_filter))
+        .map((usuario: any) => (
+          <CardUser
+            key={usuario.id}
+            id={usuario.id}
+            name={usuario.name}
+            email={usuario.email}
+            active={usuario.active}
+            img={Math.floor(Math.random() * 100)}
+          />
+        ))}
     </div>
   )
 }
 
 export default function UsersPage() {
   const [usuarios, setUsuarios] = useState<User[]>([])
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     axios
@@ -87,9 +100,20 @@ export default function UsersPage() {
   return (
     <div className={styles.userPageContainer}>
       <div className={styles.searchContainer}>
-        <SearchInput />
+        <SearchInput
+          text={searchQuery}
+          onChange={(e: any) => setSearchQuery(e.target.value)}
+        />
+        <div
+          className={styles.userCount}
+        >{`${usuarios.length} usuários encontrados`}</div>
       </div>
-      <UserList usuarios={usuarios} />
+      <UserList usuarios={usuarios} filter={searchQuery} />
+      <Link href={'/usuarios/novo'}>
+        <div className={styles.floatingButton}>
+          <AddIcon sx={{ fontSize: 40 }} />
+        </div>
+      </Link>
     </div>
   )
 }
---
+++ pages/usuarios/novo.tsx
@@ -0,0 +1,3 @@
+export default function UserNewPage() {
+  return <p>new user</p>
+}
